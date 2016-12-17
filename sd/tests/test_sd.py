c60ca98ed80bfb9a61ba387229b2eb086da35b80
Qingxue Zhang <qingxue_zhang@outlook.com>
2016-12-17 12:22:19 -0500

    fix indent

--- sd/tests/test_sd.py
+++ sd/tests/test_sd.py
@@ -2,4 +2,4 @@ import pytest
 from sd.sd import standard_deviation
 
 def test_standard_deviation():
-assert standard_deviation([1,2,3,4,5]) == 1.4142135623730951, 'pass normal case test'
+    assert standard_deviation([1,2,3,4,5]) == 1.4142135623730951, 'pass normal case test, the answer should be 1.41'
