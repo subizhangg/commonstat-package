d42ce0a294c9cf678b626ea12dbf5897bd098ef1
Qingxue Zhang <qingxue_zhang@outlook.com>
2016-12-17 12:10:36 -0500

    add pytest

---
+++ sd/tests/test_sd.py
@@ -0,0 +1,5 @@
+import pytest
+from sd import sd
+
+def test_sd.standard_deviation():
+assert sd.standard_deviation([1,2,3,4,5]) == 1.4142135623730951, 'the standard deviation of 1,2,3,4,5 is equal to 1.414'
